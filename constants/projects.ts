f9655486b375d6a7ab19df7d2b3ff35c915f7c90
hyeon9782 <hyeon9782@gmail.com>
2023-10-10 17:46:39 +0900

    feat: projects.ts 생성

---
+++ constants/projects.ts
@@ -0,0 +1,39 @@
+export const PROJECTS = [
+  {
+    name: "Portfolio",
+    explanation:
+      "현정호라는 개발자가 어떤 개발자인지 소개하는 포트폴리오 사이트입니다!",
+    skills: ["TypeScript", "Next.js"],
+    functions: ["다양한 애니메이션"],
+  },
+  {
+    name: "Travel Mate",
+    explanation: "자신만의 여행 코스를 기획할 수 있게 도움을 주는 웹 서비스",
+    skills: ["TypeScript", "React", "Node.js", "Express"],
+    functions: ["여행 코스 기획 기능", "게시글 조회 / 작성 / 수정 / 삭제 기능"],
+  },
+  {
+    name: "Just Say",
+    explanation:
+      "다양한 상황에서 영어 회화를 연습하고 영어에 대한 두려움을 극복하는 것을 목표로 개발한 반응형 웹 기반의 AI 서비스",
+    skills: ["JavaScript", "React", "Python", "FastAPI"],
+    functions: [
+      "Web Speech API를 이용한 STT (Speech to Text)기능 구현",
+      "ChatGPT API를 이용한 영어 상황극 기능 구현",
+      "AI와 나눴던 대화를 다시 볼 수 있는 기능 구현",
+    ],
+  },
+  {
+    name: "컬리의 일상",
+    explanation:
+      "식생활 및 라이프스타일 컨텐츠를 생성하고 공유함과 동시에 소비자의 재구매 유도를 목표로 개발한 커뮤니티 모바일 웹",
+    skills: ["JavaScript", "Vue", "Java", "Spring Boot"],
+    functions: [
+      "포스팅 작성 / 수정 / 삭제 기능 구현",
+      "팔로우한 포스팅 조회 기능 구현",
+      "카테고리 별 포스팅 조회 기능 구현",
+      "스크랩한 포스팅 조회 기능 구현",
+      "포스팅 검색 기능 구현",
+    ],
+  },
+];
